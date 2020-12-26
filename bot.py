d723551655463712c0868038e4804e5594d9fbb4
Crystoffar <crystoffar@gmail.com>
2020-12-26 12:36:39 -0800

    Created ehe command

--- bot.py
+++ bot.py
@@ -1,14 +1,21 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '!:')
+bot = commands.Bot(command_prefix = '!:')
 
-@client.event
+#Events
+@bot.event
 async def on_ready():
     print('Paimon is ready!')
 
+#Commands
+@bot.command()
+async def ehe(ctx):
+    await ctx.send('"Ehe" te nandayo?!')
+
+#Obtaining Token
 tokenFile = open("token.txt", "r")
 tokenStr = tokenFile.read()
 tokenFile.close()
 
-client.run(tokenStr)
+bot.run(tokenStr)
